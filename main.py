d5b3730aca914cb41010c53d5d180b137026f946
Colin Ries <colin.ries@gmail.com>
2018-12-26 05:20:09 +0100

    A* done
    A-Star search works
    -need to group together so that the different searches can be easily switched for one another

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ from pygame.locals import *
 window_x, window_y = 5, 32
 width, height = 1600, 900
 cellSize = 100
-frames_per_second = 25.0
+frames_per_second = 200
 
-start_pos = (7, 0)
+start_pos = (1, 3)
 goal_pos = (14, 4)
 
 HEURISTIC = "manhattan" # "manhattan" or "straight"
@@ -34,15 +34,15 @@ frame_time = 1000000.0 / frames_per_second
 
 
 def create_map():
-    map = []
+    new_map = []
     empty_row = []
     for i in range(0, gridHeight):
-        map.append(list(empty_row))
+        new_map.append(list(empty_row))
     for j in range(0, gridHeight):
         for i in range(0, gridWidth):
-            map[j].append({"type": " "})
+            new_map[j].append({"type": " "})
 
-    return map
+    return new_map
 
 
 def estimate_dist(start_pos, goal_pos):
@@ -62,11 +62,11 @@ def get_moves(source_pos):
     if MOVE_MODE == "cross":
         if source_pos[1] > 0:
             move_candidates.append((source_pos[0], source_pos[1]-1))
-        if source_pos[1] < gridHeight:
+        if source_pos[1] < gridHeight-1:
             move_candidates.append((source_pos[0], source_pos[1]+1))
         if source_pos[0] > 0:
             move_candidates.append((source_pos[0]-1, source_pos[1]))
-        if source_pos[0] < gridWidth:
+        if source_pos[0] < gridWidth-1:
             move_candidates.append((source_pos[0]+1, source_pos[1]))
     elif MOVE_MODE == "star":
         move_candidates = []
@@ -77,22 +77,77 @@ def get_moves(source_pos):
 def check_move_legality(map, move_candidates):
     legal_moves = []
     for move in move_candidates:
+        #print("move", move)
         if not map[move[1]][move[0]]["type"] == "X":
             legal_moves.append(move)
 
     return legal_moves
 
 
-map = create_map()
+def a_star(map, current_pos, open_list, closed_list, legal_moves):
+    #print("open_list", open_list)
+    #print("closed_list", closed_list)
+    #print("legal_moves", legal_moves)
+
+    if current_pos in open_list:
+        open_list.remove(current_pos)
+        closed_list.append(current_pos)
+
+    minimum_F = -1
+    minimum_F_cell = current_pos
+
+    for cell in legal_moves:
+        #print("cell", cell)
+        if cell in open_list:
+            map[cell[1]][cell[0]]["H"] = estimate_dist(cell, goal_pos)
+            new_G = map[current_pos[1]][current_pos[0]]["G"] + cellSize
+            new_F = map[cell[1]][cell[0]]["H"] + new_G
+
+            if ("G" in map[cell[1]][cell[0]].keys()) and (new_G < map[cell[1]][cell[0]]["G"]):
+                map[cell[1]][cell[0]]["G"] = new_G
+                map[cell[1]][cell[0]]["F"] = new_F
+                map[cell[1]][cell[0]]["parent"] = current_pos
+            elif ("G" not in map[cell[1]][cell[0]].keys()) or ("F" not in map[cell[1]][cell[0]].keys()):
+                map[cell[1]][cell[0]]["G"] = new_G
+                map[cell[1]][cell[0]]["F"] = new_F
+                map[cell[1]][cell[0]]["parent"] = current_pos
+
+    for cell in open_list:
+        if (map[cell[1]][cell[0]]["F"] < minimum_F) or (minimum_F < 0):
+            minimum_F = map[cell[1]][cell[0]]["F"]
+            minimum_F_cell = cell
+
+    current_pos = minimum_F_cell
+    open_list.remove(current_pos)
+    closed_list.append(current_pos)
+
+    return map, current_pos, open_list, closed_list
 
 
-map[start_pos[1]][start_pos[0]]["type"] = "S"
-map[goal_pos[1]][goal_pos[0]]["type"] = "F"
+game_map = create_map()
 
+
+game_map[start_pos[1]][start_pos[0]]["type"] = "S"
+game_map[goal_pos[1]][goal_pos[0]]["type"] = "F"
+
+for i in range(2, 14):
+    game_map[7][i]["type"] = "X"
+for i in range(0, 2):
+    game_map[i][3]["type"] = "X"
+for i in range(4, 7):
+    game_map[i][3]["type"] = "X"
+for i in range(1, 7):
+    game_map[i][5]["type"] = "X"
 for i in range(0, 6):
-    map[1+i][7]["type"] = "X"
+    game_map[i][7]["type"] = "X"
+for i in range(1, 7):
+    game_map[i][9]["type"] = "X"
+for i in range(0, 6):
+    game_map[i][11]["type"] = "X"
+for i in range(1, 7):
+    game_map[i][13]["type"] = "X"
 
-print_map(map)
+print_map(game_map)
 
 os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (window_x, window_y)
 pygame.init()
@@ -110,27 +165,44 @@ for i in range(0, gridHeight):
 
 for row in range(0, gridHeight):
     for cell in range(0, gridWidth):
-        if map[row][cell]["type"] == "S":
+        if game_map[row][cell]["type"] == "S":
             pygame.draw.rect(screen, GREEN, (cell*cellSize+1, row*cellSize+1, cellSize-2, cellSize-2), 0)
-        elif map[row][cell]["type"] == "F":
+        elif game_map[row][cell]["type"] == "F":
             pygame.draw.rect(screen, RED, (cell*cellSize+1, row*cellSize+1, cellSize-2, cellSize-2), 0)
-        elif map[row][cell]["type"] == "X":
+        elif game_map[row][cell]["type"] == "X":
             pygame.draw.rect(screen, WHITE, (cell*cellSize+1, row*cellSize+1, cellSize-2, cellSize-2), 0)
 
 pygame.display.flip()
 
 current_pos = start_pos
 
-move_candidates = get_moves(current_pos)
-print("move_candidates", move_candidates)
+# A-Star init
+open_list = [start_pos]
+closed_list = []
+game_map[start_pos[1]][start_pos[0]]["G"] = 0
 
-legal_moves = check_move_legality(map, move_candidates)
-print("legal_moves", legal_moves)
+arrived_at_goal = False
 
-
-while True:
+while not arrived_at_goal:
     timestamp_frame_start = datetime.now()
 
+    #print("current_pos", current_pos)
+
+    move_candidates = get_moves(current_pos)
+    #print("move_candidates", move_candidates)
+
+    legal_moves = check_move_legality(game_map, move_candidates)
+    #print("legal_moves", legal_moves)
+
+    for cell in legal_moves:
+        if (cell not in open_list) and (cell not in closed_list):
+            open_list.append(cell)
+
+    game_map, current_pos, open_list, closed_list = a_star(game_map, current_pos, open_list, closed_list, legal_moves)
+
+    if current_pos == goal_pos:
+        arrived_at_goal = True
+        #print("arrived")
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -141,4 +213,27 @@ while True:
     if frame_build_time < frame_time:
         time.sleep((frame_time - frame_build_time) / 1000000.0)
     pygame.display.flip()
+
+parent = goal_pos
+total_path = []
+while True:
+    print(parent, " <-- ", game_map[parent[1]][parent[0]]["parent"])
+    parent = game_map[parent[1]][parent[0]]["parent"]
+    if parent == start_pos:
+        break
+    else:
+        total_path.append(parent)
+
+print("total_path", total_path)
+
+for path_cell in total_path:
+    #print("path_cell", path_cell)
+    pygame.draw.rect(screen, BLUE, (path_cell[0] * cellSize + 1, path_cell[1] * cellSize + 1, cellSize - 2, cellSize - 2), 0)
+    pygame.display.flip()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit(0)
 
