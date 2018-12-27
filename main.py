5268a58ea78ba353b6e64debe11505c8a77616b7
Colin Ries <colin.ries@gmail.com>
2018-12-28 00:59:22 +0100

    Editing maps working
    running with SPACE
    svaing with ENTER
    draw start with S+LMB
    draw finish with F+LMB
    draw walls by clicking LMB
    trace long walls by clicking and holding LMB

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ window_x, window_y = 5, 32
 # width, height = 1600, 900
 # cellSize = 100
 moves_per_second = 10
+fast_speed = 200
+real_speed = moves_per_second
 
 # start_pos = (0, 3)
 # goal_pos = (15, 0)
@@ -257,8 +259,12 @@ def export_map_structure(map):
                            "cellSize": cellSize, "windowHeight": height, "windowWidth": width, "startPos": start_pos,
                            "goalPos": goal_pos}
 
-    with open("mymap.json", "w") as fout:
-        json.dump(map_compressed_dict, fout)
+    if len(argv) > 1:
+        with open(argv[1], "w") as fout:
+            json.dump(map_compressed_dict, fout)
+    else:
+        with open("new_map.json", "w") as fout:
+            json.dump(map_compressed_dict, fout)
 
 
 def import_map_structure(map_compressed_dict):
@@ -338,6 +344,17 @@ def reset_map_vars(map):
 # final_map_dict = export_map_structure(game_map)
 # import_map_structure(final_map_dict)
 
+# Default values
+height = 900
+width = 1600
+gridWidth = 16
+gridHeight = 9
+cellSize = 100
+start_pos = (0, 0)
+goal_pos = (gridWidth - 1, gridHeight - 1)
+current_pos = start_pos
+game_map = create_map(gridWidth, gridHeight)
+
 if len(argv) > 1:
     filename = argv[1]
     if filename != "":
@@ -353,16 +370,8 @@ if len(argv) > 1:
             width = gridWidth * cellSize
             height = gridHeight * cellSize
             game_map = create_map(gridWidth, gridHeight)
-else:
-    # Default values
-    height = 900
-    width = 1600
-    gridWidth = 16
-    gridHeight = 9
-    cellSize = 100
-    start_pos = (0, 0)
-    goal_pos = (gridWidth - 1, gridHeight - 1)
-    current_pos = start_pos
+            start_pos = (0, 0)
+            goal_pos = (gridWidth-1, gridHeight-1)
 
 os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (window_x, window_y)
 pygame.init()
@@ -398,8 +407,17 @@ arrived_at_goal = False
 dead_end = False
 signal_dead_end = False
 path_traced = False
+set_new_start = False
+set_new_goal = False
+trace_new_wall = False
+open_list = []
+closed_list = []
+new_wall_blocks_list = []
+dead_end_pos = (0, 0)
 
 # print_map(game_map)
+
+draw_map(game_map, gridWidth, gridHeight, cellSize, width, height)
 
 while True:
     timestamp_frame_start = datetime.now()
@@ -496,37 +514,87 @@ while True:
 
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RETURN:
+            if event.key == pygame.K_SPACE:
                 draw_map(game_map, gridWidth, gridHeight, cellSize, width, height)
                 live_color = DARK_GREY
                 search_path = True
+            elif event.key == pygame.K_RETURN:
+                export_map_structure(game_map)
             elif event.key == pygame.K_1:
                 SEARCH_ALGO = "hillclimb"
             elif event.key == pygame.K_2:
                 SEARCH_ALGO = "bestfirst"
             elif event.key == pygame.K_3:
                 SEARCH_ALGO = "a_star"
-            elif event.key == pygame.K_PLUS or event.key == pygame.K_KP_PLUS:
+            elif event.key == pygame.K_PLUS or event.key == pygame.K_KP_PLUS or event.key == pygame.K_UP:
                 if moves_per_second >= 10:
                     moves_per_second += 10
                 else:
                     moves_per_second += 1
-            elif event.key == pygame.K_MINUS or event.key == pygame.K_KP_MINUS:
+            elif event.key == pygame.K_MINUS or event.key == pygame.K_KP_MINUS or event.key == pygame.K_DOWN:
                 if moves_per_second > 10:
                     moves_per_second -= 10
                 elif moves_per_second > 1:
                     moves_per_second -= 1
+            if event.key == pygame.K_s:
+                set_new_start = True
+            elif event.key == pygame.K_f:
+                set_new_goal = True
 
             frame_time = 1000000.0 / moves_per_second
 
-            pygame.display.set_caption("Search algo: %s        Moves/Second: %s" %(SEARCH_ALGO, moves_per_second))
+            pygame.display.set_caption("Search algo: %s        Moves/Second: %s" % (SEARCH_ALGO, moves_per_second))
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            pressed1, pressed2, pressed3 = pygame.mouse.get_pressed()
+
+            if pressed1 and set_new_start:
+                game_map[start_pos[1]][start_pos[0]]["type"] = " "
+                start_pos = pygame.mouse.get_pos()
+                start_pos = (start_pos[0] // cellSize, start_pos[1] // cellSize)
+                game_map[start_pos[1]][start_pos[0]]["type"] = "S"
+                set_new_start = False
+            elif pressed1 and set_new_goal:
+                game_map[goal_pos[1]][goal_pos[0]]["type"] = " "
+                goal_pos = pygame.mouse.get_pos()
+                goal_pos = (goal_pos[0] // cellSize, goal_pos[1] // cellSize)
+                game_map[goal_pos[1]][goal_pos[0]]["type"] = "F"
+                set_new_goal = False
+            elif pressed1:
+                trace_new_wall = True
+                new_wall_blocks_list = []
+
+            draw_map(game_map, gridWidth, gridHeight, cellSize, width, height)
 
         if event.type == pygame.QUIT:
             pygame.quit()
             exit(0)
 
+    pressed1, pressed2, pressed3 = pygame.mouse.get_pressed()
+
+    if pressed1 and trace_new_wall:
+        # print("tracing mouse")
+        wall_toggle_pos = pygame.mouse.get_pos()
+        wall_toggle_pos = (wall_toggle_pos[0] // cellSize, wall_toggle_pos[1] // cellSize)
+        if wall_toggle_pos not in new_wall_blocks_list:
+            new_wall_blocks_list.append(wall_toggle_pos)
+        # print("new_wall_blocks_list", new_wall_blocks_list)
+        real_speed = moves_per_second
+        moves_per_second = fast_speed
+
+    if trace_new_wall and not pressed1:
+        trace_new_wall = False
+        for block in new_wall_blocks_list:
+            if game_map[block[1]][block[0]]["type"] == " ":
+                game_map[block[1]][block[0]]["type"] = "X"
+            elif game_map[block[1]][block[0]]["type"] == "X":
+                game_map[block[1]][block[0]]["type"] = " "
+
+        moves_per_second = real_speed
+        frame_time = 1000000.0 / moves_per_second
+        draw_map(game_map, gridWidth, gridHeight, cellSize, width, height)
+
     frame_build_time = (datetime.now() - timestamp_frame_start).microseconds
     if frame_build_time < frame_time:
         time.sleep((frame_time - frame_build_time) / 1000000.0)
     pygame.display.flip()
-
